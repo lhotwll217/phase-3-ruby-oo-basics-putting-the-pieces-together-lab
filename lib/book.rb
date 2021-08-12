b0b14596c4cd6cca1920eee53ac814bb5969107d
lhotwll217 <lukehenryotwell@gmail.com>
2021-08-12 03:00:00 -0400

    commit tree 0b0f93e033c919d26307d290a14d8284a619bde5

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
 
+class Book
 
+
+    attr_accessor :author, :genre, :page_count
+    
+    def title 
+    @title 
+    end
+
+    def initialize(title)
+    @title = title
+    end
+
+    def turn_page 
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
-# Make your shoe class here!
+class Shoe 
+
+    attr_accessor :color, :size, :material, :condition
+    
+
+    def initialize(brand) 
+        @brand = brand
+    end
+
+    attr_reader :brand
+
+    def cobble
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+    end
+end
